ecc21950ccfa8a2d37f323ff61ce37b52760baeb
omar <omaralkadri4@gmail.com>
2026-05-04 23:11:15 +0200

    defining functions for chief charms

--- scripts/charms.js
+++ scripts/charms.js
@@ -1,7 +1,26 @@
+const basePoint = 70;
+let baseScore;
+
 fetch("./costs/charms.json")
 .then(response => response.json())
 .then(data => {
-    console.log(data);
+    baseScore = data;
+
+    console.log(baseScore);
+    console.log(calculateCosts(0, 3))
+    
 })
 .catch(error => console.log(error
 ));
+
+
+function calculateCosts(from, to) {
+    let i = from + 1;
+    let totalCosts = 0
+    while (i <= to){
+        totalCosts += baseScore.cost["lvl"+i].guide;
+        i++;
+    }
+    return totalCosts;
+}
+
